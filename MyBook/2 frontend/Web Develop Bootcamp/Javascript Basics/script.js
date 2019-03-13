b5772e16ba73b600912b4d24f4522fe3410c044b
Kevin Dai <lexdxw@126.com>
2019-03-13 22:49:49 +0800

    添加Javascript 基础

---
+++ MyBook/2 frontend/Web Develop Bootcamp/Javascript Basics/script.js
@@ -0,0 +1,16 @@
+// var firstName = prompt("what is your first name?");
+// var lastName = prompt("what is your last name?");
+// var _age = prompt("how old are you?");
+// console.log("Nice to meet you " + firstName + " " + lastName);
+// console.log("you are " + _age + " years old ");
+
+var _age = prompt("how old are you?")*365;
+console.log(_age + " days you've been alive");
+
+let i =0
+
+while(i<100)
+{
+    console.log(i);
+    i++;
+}
