91346d08c7eafb692c7c6e52e004a8b364401b8b
Fi-ce777 <824904372@qq.com>
2019-04-03 21:56:06 +0800

    图像传输系列 初始化

---
+++ ImageTransmission/ImageUploader/ImageUploader/DAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ImageUploader
+{
+	class DAO
+	{
+		public class Settings
+		{
+
+		}
+
+		public class ImageData
+		{
+			public ImageData()
+			{
+				m_hImage = new BitmapImage();
+				m_hImageStream = new MemoryStream();
+			}
+			public BitmapImage m_hImage;
+			public MemoryStream m_hImageStream;
+		}
+
+		public DAO()
+		{
+			m_hImageData = new ImageData();
+			m_hSettings = new Settings();
+		}
+
+		public Settings m_hSettings;
+		public ImageData m_hImageData;
+	}
+}
---
+++ ImageTransmission/ImageUploader/ImageUploader/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageUploader
+{
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		Timer m_hImageUpdateTimer;
+		DAO m_hDao;
+		
+		public MainWindow()
+		{
+			m_hDao = new DAO();
+
+			InitializeComponent();
+
+			m_hImageUpdateTimer = new Timer();
+			m_hImageUpdateTimer.Elapsed += new ElapsedEventHandler(Onm_hImageUpdateTimerEvent);
+			m_hImageUpdateTimer.Interval = 1000;
+			m_hImageUpdateTimer.Start();
+
+			m_hImageShow.Source = m_hDao.m_hImageData.m_hImage;
+		}
+
+		private void Onm_hImageUpdateTimerEvent(object source, ElapsedEventArgs e)
+		{
+			RenderTargetBitmap bmp = new RenderTargetBitmap(100, 100, 96, 96, PixelFormats.Pbgra32); ;
+			DrawingVisual drawingVisual = new DrawingVisual();
+			DrawingContext drawingContext = drawingVisual.RenderOpen();
+
+			Pen pen = new Pen();
+			pen.Brush = new SolidColorBrush(Color.FromRgb(255, 255, 0));
+			pen.Thickness = 5;
+			drawingContext.DrawLine(pen, new Point(0, 0), new Point(100, 100));
+
+			drawingContext.Close();
+			bmp.Render(drawingVisual);
+
+			PngBitmapEncoder bitmapEncoder = new PngBitmapEncoder();
+			bitmapEncoder.Frames.Add(BitmapFrame.Create(bmp));
+			bitmapEncoder.Save(m_hDao.m_hImageData.m_hImageStream);
+			m_hDao.m_hImageData.m_hImageStream.Seek(0, SeekOrigin.Begin);
+
+			Dispatcher.Invoke(new Action(delegate
+			{
+				m_hDao.m_hImageData.m_hImage.BeginInit();
+				m_hDao.m_hImageData.m_hImage.CacheOption = BitmapCacheOption.OnLoad;
+				m_hDao.m_hImageData.m_hImage.StreamSource = m_hDao.m_hImageData.m_hImageStream;
+				m_hDao.m_hImageData.m_hImage.EndInit();
+			}));
+		}
+	}
+}
