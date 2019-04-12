8e2108a1c87108c197b8a3fbcb4fb2f9a8330c2e
Kevin Dai <lexdxw@126.com>
2019-04-12 14:28:08 +0800

    添加TodosAPI express项目

--- TodosAPI/index.js
+++ TodosAPI/index.js
@@ -3,13 +3,17 @@ var express = require("express"),
 	env_port = 80;
 
 var todoRoutes = require("./routes/todos"); //导入Router选择路径
-	
+var bodyparser = require("body-parser");
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
 app.get('/',function(req,res){
 	res.send("Hi there,from root routes!");
 
 });
+app.use('/api/todos',todoRoutes); //Router复杂路径选择功能
 
-app.use('/api/test',todoRoutes); //Router复杂路径选择功能
 	
 app.listen(env_port,function(){
 	console.log("System is running on " + env_port);
--- TodosAPI/routes/todos.js
+++ TodosAPI/routes/todos.js
@@ -4,12 +4,45 @@ var express = require("express"),
 var db = require("../models");
 
 
-router.get("/hi",function (req,res) {
+router.get("/",function (req,res) {
     db.Todo.find().then(function (todos) {
         res.json(todos);
     }).catch(function (err) {
         res.send(err);
     })
+});
+
+router.post('/',function (req,res) {
+    db.Todo.create(req.body)
+        .then(function (newTodo) {
+            res.status(201)/*设置返回状态码*/.json(newTodo);
+        }).catch(function (err) {
+            res.send(err);
+    })
+});
+
+router.get('/:todoId',function (req,res) {
+   db.Todo.findById(req.params.todoId).then(function (foundTodo) {
+       res.json(foundTodo);
+   }).catch(function (err) {
+       res.send(err);
+   })
+});
+
+router.put('/:todoId',function (req,res) {
+    db.Todo.findOneAndUpdate({_id:req.params.todoId},req.body,{new:true} /*更新后的数据立即返回至promise*/).then(function (foundTodo) {
+        res.json(foundTodo);
+    }).catch(function (err) {
+        res.send(err);
+    });
+});
+
+router.delete('/:todoId',function (req,res) {
+   db.Todo.remove({_id:req.params.todoId}).then(function () {
+       res.json({message:"We delete it!"});
+   }).catch(function (err) {
+       res.send(err);
+   })
 });
 
 
