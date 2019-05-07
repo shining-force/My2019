1b5b6cab66490f521a04d9fea332ee0003ac9b84
Kevin Dai <lexdxw@126.com>
2019-05-07 08:37:02 +0800

    更新my-company-frontend

---
+++ MyCompany/my-company-frontend/my-app/src/NavBarComponent.js
@@ -0,0 +1,10 @@
+import './NavBarComponent.css';
+import * as React from "react";
+
+class NavBarComponent extends React.Component{
+    render(){
+        return(<h1>NavBar</h1>)
+    }
+}
+
+export default NavBarComponent;
