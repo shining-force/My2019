284e1a0c0414476504331791f34951e4225deea4
Kevin Dai <lexdxw@126.com>
2019-05-29 17:20:20 +0800

    添加React基础

--- MyCompany/my-company-frontend/my-app/src/components/navBar/NavBar.js
+++ MyCompany/my-company-frontend/my-app/src/components/navBar/NavBar.js
@@ -10,7 +10,12 @@ class NavBar extends React.Component{
 
     render(){
         return(<div className="navBar">
-
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
         </div>)
     }
 }
